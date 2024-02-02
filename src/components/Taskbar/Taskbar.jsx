bc484c41347c16d38ae5c33a3b8ce1744b614420
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-02 00:55:52 -0800

    Updated to use Vite

--- src/components/Taskbar/Taskbar.jsx
+++ src/components/Taskbar/Taskbar.jsx
@@ -1,15 +1,12 @@
-import './Taskbar.scss';
-import TaskbarTime from './TaskbarTime';
-import TaskbarWindow from './TaskbarWindow';
+import "./Taskbar.scss";
+import TaskbarTime from "./TaskbarTime";
+import TaskbarWindow from "./TaskbarWindow";
+import Start from "../../assets/start.png";
 
 const Taskbar = ({ apps, focusedApp, taskbarWindowClicked }) => {
   return (
-    <div className='taskbar taskbar-gradient z-10 flex h-[30px] flex-row'>
-      <img
-        src={require('../../assets/start.png')}
-        alt='start'
-        className='mr-[10px] h-full'
-      />
+    <div className="taskbar taskbar-gradient z-10 flex h-[30px] flex-row">
+      <img src={Start} alt="start" className="mr-[10px] h-full" />
       {apps.map((app) => (
         <TaskbarWindow
           app={app}
